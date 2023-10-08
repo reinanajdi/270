0ca4cf17c7f938150c4055cc0ac4abd7e1452d21
Reina <103151477+reinanajdi@users.noreply.github.com>
2023-10-08 19:26:44 +0300

    Update spells_game.c

--- spells_game.c
+++ spells_game.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
-
-int coinToss() {
-    return rand() % 2;
+int isValidSpell(char* spell, char* lastSpell) {
+    if (spell[0] != lastSpell[strlen(lastSpell) - 1]) {
+        return 0; // Invalid spell
+    }
+    return 1; //if valid
 }
 
 int main() {
-
-    srand(time(NULL));
-
     char spells[100][100];
     int numofspells;
     FILE *spellsp;
@@ -26,6 +24,7 @@ int main() {
         fscanf(spellsp, "%s", spells[i]);
         printf("%s\n", spells[i]);
     }
+
     char player1Name[20];
     char player2Name[20];
     printf("Name of player 1: ");
@@ -35,25 +34,54 @@ int main() {
 
     char currentPlayer[20];
     char currentSpell[100];
-    int currentPlayerNumber = 0;
+    char lastSpell[100] = ""; 
 
-    int coinResult;
-    printf("%s, call heads or tails (h/t) for the coin toss: ", player1Name);
-    char call[2];
-    scanf("%s", call);
-
-    
-    coinResult = coinToss();
-    if ((coinResult == 0 && strcmp(call, "h") == 0) || (coinResult == 1 && strcmp(call, "t") == 0)) {
-        currentPlayerNumber = 0;
-    } else {
-        currentPlayerNumber = 1;
-    }
-
-    strcpy(currentPlayer, currentPlayerNumber == 0 ? player1Name : player2Name);
+    strcpy(currentPlayer, player1Name); // Player 1 starts
 
     fclose(spellsp);
 
-    return 0;
-    
+    // turn taking impl
+    while (1) {
+        printf("%s, choose a spell: ", currentPlayer);
+        scanf("%s", currentSpell);
+
+        // is the word valid
+        if (strlen(lastSpell) > 0 && !isValidSpell(currentSpell, lastSpell)) {
+            printf("Invalid spell! %s wins.\n", currentPlayer == player1Name ? player2Name : player1Name);
+            break;
+        }
+
+        // we check if spell is in the list of spells
+        int validSpell = 0;
+        for (int i = 0; i < numofspells; i++) {
+            if (strcmp(spells[i], currentSpell) == 0) {
+                validSpell = 1;
+                strcpy(spells[i], ""); //we rmeove the spell form the list 
+                break;
+            }
+        }
+
+        if (!validSpell) {
+    if (strcmp(currentPlayer, player1Name)) {
+        printf("%s chose an invalid spell! %s wins.\n", currentPlayer, player1Name);
+    } else {
+        printf("%s chose an invalid spell! %s wins.\n", currentPlayer, player2Name);
+    }
+    break;
 }
+
+
+        // Set last spell
+        strcpy(lastSpell, currentSpell);
+
+        // Switch players
+        if (strcmp(currentPlayer, player1Name) == 0) {
+            strcpy(currentPlayer, player2Name);
+        } else {
+            strcpy(currentPlayer, player1Name);
+        }
+    }
+
+    return 0;
+}
+
