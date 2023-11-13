243c0b5624e3a8535f86dc351a129d84743e0710
Reina <103151477+reinanajdi@users.noreply.github.com>
2023-11-13 05:55:36 +0200

    Update spells_game.c

--- spells_game.c
+++ spells_game.c
@@ -3,53 +3,67 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Function to check if the chosen spell is valid
 int isValidSpell(char *spell, char *lastSpell) {
-    if (spell[0] != lastSpell[strlen(lastSpell) - 1]) {
+    if (strlen(lastSpell) > 0 && tolower(spell[0]) != tolower(lastSpell[strlen(lastSpell) - 1])) {
         return 0; // Invalid spell
     }
     return 1; // Valid spell
 }
 
-int isRepetition(char* spell, char chosenSpells[][100], int currentChosenSpell) {
-    for (int i = 0; i < currentChosenSpell; i++) {
-        if (strcmp(chosenSpells[i], spell) == 0) {
+// Function to check if the chosen spell is a repetition
+int isRepetition(char *spell, char spells[][100], int numofspells) {
+    for (int i = 0; i < numofspells; i++) {
+        if (strlen(spells[i]) > 0 && strcasecmp(spells[i], spell) == 0) {
             return 1; // Repetition
         }
     }
-    return 0;
+    return 0; // Not a repetition
 }
 
-
-int checkSpellValidity(char *spell, char *lastSpell, char spells[][100], int numofspells) {
-    if (strlen(lastSpell) > 0 && !isValidSpell(spell, lastSpell)) {
-        return 0; // Invalid spell
-    } else if (isRepetition(spell, spells, numofspells)) {
-        return 1; // Repetition
-    } else {
-        return 2; // Valid spell
+// Function to check if the chosen spell is in the list of valid spells
+int isSpellInList(char *spell, char spells[][100], int numofspells) {
+    for (int i = 0; i < numofspells; i++) {
+        if (strlen(spells[i]) > 0 && strcmp(spells[i], spell) == 0) {
+            return 1; // Spell is in the list
+        }
     }
+    return 0; // Spell is not in the list
+}
+
+// Function to check if choosing a specific spell leads to a dead end
+int leadsToDeadEnd(char spells[][100], int numofspells, int spellIndex, char chosenSpells[][100], int currentChosenSpell) {
+    char lastLetter = spells[spellIndex][strlen(spells[spellIndex]) - 1];
+
+    for (int i = 0; i < numofspells; i++) {
+        if (strlen(spells[i]) > 0 && spells[i][0] == lastLetter && !isRepetition(spells[i], chosenSpells, currentChosenSpell + 1)) {
+            return 0; // There is a valid option, does not lead to a dead end
+        }
+    }
+
+    return 1; // Leads to a dead end
 }
 
 // Function to choose a spell for the bot based on the difficulty level
-char* chooseSmartBotSpell(char spells[][100], int numofspells, char lastSpell[100], int difficulty, char chosenSpells[][100], int currentChosenSpell) {
-    char* chosenSpell = "-----";
+char *chooseSmartBotSpell(char spells[][100], int numofspells, char lastSpell[100], int difficulty, char chosenSpells[][100], int currentChosenSpell) {
+    char *chosenSpell = malloc(100 * sizeof(char)); // Dynamic memory allocation
 
     if (difficulty == 0) {
-        // Easy: Choose the first word that matches the last character and is not repeated
+        // Easy: Choose the first word that matches the last character
         for (int i = 0; i < numofspells; i++) {
-            if (strlen(spells[i]) > 0 && isValidSpell(spells[i], lastSpell) && !isRepetition(spells[i], chosenSpells, currentChosenSpell)) {
-                chosenSpell = spells[i];
+            if (strlen(spells[i]) > 0 && isValidSpell(spells[i], lastSpell) && !isRepetition(spells[i], chosenSpells, currentChosenSpell + 1)) {
+                strcpy(chosenSpell, spells[i]);
                 break;
             }
         }
     } else if (difficulty == 1) {
-        // Medium: Randomly choose a word that starts with the last character of human input and is not repeated
+        // Medium: Randomly choose a word that starts with the last character of human input
         char lastLetter = lastSpell[strlen(lastSpell) - 1];
         int validOptions[100];
         int numValidOptions = 0;
 
         for (int i = 0; i < numofspells; i++) {
-            if (strlen(spells[i]) > 0 && spells[i][0] == lastLetter && !isRepetition(spells[i], chosenSpells, currentChosenSpell)) {
+            if (strlen(spells[i]) > 0 && spells[i][0] == lastLetter && !isRepetition(spells[i], chosenSpells, currentChosenSpell + 1)) {
                 validOptions[numValidOptions++] = i;
             }
         }
@@ -57,39 +71,44 @@ char* chooseSmartBotSpell(char spells[][100], int numofspells, char lastSpell[10
         if (numValidOptions > 0) {
             // Choose a random spell from the valid options
             int randomIndex = rand() % numValidOptions;
-            chosenSpell = spells[validOptions[randomIndex]];
+            strcpy(chosenSpell, spells[validOptions[randomIndex]]);
         }
     } else if (difficulty == 2) {
-        // Hard: Choose words that lead to a dead end without repetition
+        // Hard: Choose words that lead to a dead end, or randomly choose a word that starts with the last character of human input
         char lastLetter = lastSpell[strlen(lastSpell) - 1];
         int validOptions[100];
         int numValidOptions = 0;
 
         for (int i = 0; i < numofspells; i++) {
-            if (strlen(spells[i]) > 0 && spells[i][0] == lastLetter) {
-                int leadsToDeadEnd = 1;
-                for (int j = 0; j < numofspells; j++) {
-                    if (strlen(spells[j]) > 0 && spells[j][0] == spells[i][strlen(spells[i]) - 1] && i != j) {
-                        leadsToDeadEnd = 0;
-                        break;
-                    }
-                }
-                if (leadsToDeadEnd && !isRepetition(spells[i], chosenSpells, currentChosenSpell)) {
+            if (strlen(spells[i]) > 0 && !isRepetition(spells[i], chosenSpells, currentChosenSpell + 1)) {
+                if (spells[i][0] == lastLetter || (spells[i][0] != lastLetter && leadsToDeadEnd(spells, numofspells, i, chosenSpells, currentChosenSpell + 1))) {
                     validOptions[numValidOptions++] = i;
                 }
             }
         }
 
         if (numValidOptions > 0) {
-            // Choose a word that leads to a dead end
+            // Choose a word that leads to a dead end or randomly starts with the last character of human input
             int randomIndex = rand() % numValidOptions;
-            chosenSpell = spells[validOptions[randomIndex]];
+            strcpy(chosenSpell, spells[validOptions[randomIndex]]);
+        }
+    }
+
+    // Remove the chosen spell from the list of available spells
+    for (int i = 0; i < numofspells; i++) {
+        if (strcmp(spells[i], chosenSpell) == 0) {
+            spells[i][0] = '\0'; // Empty the spell to mark it as chosen
+            break;
         }
     }
 
     return chosenSpell;
 }
 
+// Function to simulate a coin toss (heads or tails)
+int coinToss() {
+    return rand() % 2; // Returns 0 for heads (player goes first) or 1 for tails (bot goes first)
+}
 
 int main() {
     int difficulty;
@@ -132,17 +151,29 @@ int main() {
 
     char currentSpell[100];
     char lastSpell[100] = "";
+    srand(time(NULL)); // Seed the random number generator with the current time
 
-    srand(time(NULL));
+    int isHumanPlayerTurn = 1; // 1 for human, 0 for bot; Initialize as human player's turn
+    int coinResult = coinToss();
 
-    int isHumanPlayerTurn = 0;
-    printf("%s, choose a spell: ", player1Name);
-    scanf("%s", lastSpell);
+    if (coinResult == 1) {
+        printf("Heads! %s goes first.\n", player1Name);
+        isHumanPlayerTurn = 1; // Human player goes first (heads)
+    } else {
+        printf("Tails! Bot goes first.\n");
+        isHumanPlayerTurn = 0; // Bot goes first (tails)
+    }
 
     while (1) {
         if (isHumanPlayerTurn) {
             printf("%s, choose a spell: ", player1Name);
             scanf("%s", currentSpell);
+
+            // Check if the player's spell is in the list
+            if (!isSpellInList(currentSpell, spells, numofspells)) {
+                printf("Invalid spell! %s loses.\n", player1Name);
+                break;
+            }
         } else {
             strcpy(currentSpell, chooseSmartBotSpell(spells, numofspells, lastSpell, difficulty, chosenSpells, currentChosenSpell));
             printf("Bot chooses: %s\n", currentSpell);
